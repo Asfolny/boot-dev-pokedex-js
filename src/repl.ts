2faa1cd0a6493f6d62f5eba38ddc46e37de4abfa
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-13 21:02:58 +0200

    Add basic repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline";
+
+export function startREPL(): void {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > "
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    const clean = cleanInput(line);
+    if (clean.length < 1) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${clean[0]}`);
+    rl.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
   return input.split(" ").filter(n => n.length > 0);
 }
