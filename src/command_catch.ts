04b3838f78cf319049fcd85b52b287f046a49641
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-14 22:02:46 +0200

    Rewrote function adding in pokedex save
    I had different ideas here, and I just ended up being easier to rewrite

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -2,17 +2,21 @@ import type { State } from "./state.js";
 
 export async function commandCatch(state: State, ...args: string[]) {
   if (args.length !== 1) {
-    console.log(args.length);
-    console.error("Catch needs a pokemon to catch")
+    throw new Error("you must provide a pokemon name");
   }
 
-  const pokemon = args[0];
-  console.log(`Throwing a Pokeball at ${pokemon}...`);
-  const chance = Math.random() * 10;
-  if (chance < 5) {
-    console.log(`${pokemon} escaped!`);
+  const name = args[0];
+  const pokemon = await state.pokeAPI.fetchPokemon(name);
+
+  console.log(`Throwing a Pokeball at ${pokemon.name}...`);
+
+  const res = Math.floor(Math.random() * pokemon.base_experience);
+  if (res > 40) {
+    console.log(`${pokemon.name} escaped!`);
     return;
   }
 
-  console.log(`${pokemon} was caught!`);
+  console.log(`${pokemon.name} was caught!`);
+  console.log("You may now inspect it with the inspect command.");
+  state.pokedex[pokemon.name] = pokemon;
 }
