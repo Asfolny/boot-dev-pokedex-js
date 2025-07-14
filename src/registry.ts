4fcb62828bc8cc8eb1b32b08445cbd308f1db356
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-14 22:12:12 +0200

    Register pokedex command

--- src/registry.ts
+++ src/registry.ts
@@ -4,6 +4,7 @@ import { commandMapForward, commandMapBack } from "./command_map.js";
 import { commandExplore } from "./command_explore.js";
 import { commandCatch } from "./command_catch.js";
 import { commandInspect } from "./command_inspect.js";
+import { commandPokedex } from "./command_pokedex.js";
 import { type CLICommand } from "./state.js";
 
 export function getCommands(): Record<string, CLICommand> {
@@ -42,6 +43,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "inspect <pokemon>",
       description: "View details about a caught pokemon",
       callback: commandInspect,
+    },
+    pokedex: {
+      name: "pokedex",
+      description: "Print current caught and registered pokemon",
+      callback: commandPokedex,
     },
   };
 }
