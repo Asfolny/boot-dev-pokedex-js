c4050df9b4a770ab9218e753aaf018dddf70795e
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-14 20:35:38 +0200

    Add mabp command

--- src/command_map.ts
+++ src/command_map.ts
@@ -11,3 +11,17 @@ export async function commandMapForward(state: State) {
   }
 }
 
+export async function commandMapBack(state: State) {
+  if (!state.prevLocationsURL) {
+    throw new Error("you're on the first page");
+  }
+
+  const locations = await state.pokeAPI.fetchLocations(state.prevLocationsURL);
+
+  state.nextLocationsURL = locations.next;
+  state.prevLocationsURL = locations.previous;
+
+  for (const loc of locations.results) {
+    console.log(loc.name);
+  }
+}
