c807a2cf95749cd9a7bd5c7258d35808f1e97a78
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-14 21:58:42 +0200

    Register catch command

--- src/registry.ts
+++ src/registry.ts
@@ -2,6 +2,7 @@ import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
 import { commandMapForward, commandMapBack } from "./command_map.js";
 import { commandExplore } from "./command_explore.js";
+import { commandCatch } from "./command_catch.js";
 import { type CLICommand } from "./state.js";
 
 export function getCommands(): Record<string, CLICommand> {
@@ -30,6 +31,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "explore",
       description: "Explore the location given",
       callback: commandExplore,
+    },
+    catch: {
+      name: "catch",
+      description: "Catch a pokemon",
+      callback: commandCatch,
     },
   };
 }
